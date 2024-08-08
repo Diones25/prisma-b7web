6541d5852b15bb343bc603f4e7aa72a0fcaf1f72
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-08-08 11:05:02 -0300

    removendo código não utilizado

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import helmet from 'helmet';
 import path from 'path';
 import router from './routes/routes';
-import { errorHandler, notFoundRequest } from './routes/errorhandler';
 
 const server = express();
 
@@ -12,9 +11,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, '../public')));
 server.use(router);
-
-server.use(notFoundRequest);
-server.use(errorHandler);
 
 server.listen(process.env.PORT, () => {
   console.log(`Servidor rodando: http://localhost:${process.env.PORT}`)
